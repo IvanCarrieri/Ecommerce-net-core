118c8f4586d8aade9007bff6b9dd91cf1f854a19
IvanCarrieri <ivancarrieri@yahoo.com.ar>
2023-11-07 15:52:31 -0300

    Orden de proyecto

---
+++ SistemaInventario.Modelos/ViewModel/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaInventario.Modelos.ViewModel
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
