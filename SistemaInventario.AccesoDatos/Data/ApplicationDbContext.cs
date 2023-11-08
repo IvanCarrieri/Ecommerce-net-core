c4953088c4dca4d7c2a11a1317c5ba8bb1aa8e2a
IvanCarrieri <ivancarrieri@yahoo.com.ar>
2023-11-08 11:16:08 -0300

    configuracíon del proyecto

--- SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Modelos;
+using System.Reflection;
 
 namespace SistemaInventario.AccesoDatos.Data
 {
@@ -9,5 +11,13 @@ namespace SistemaInventario.AccesoDatos.Data
             : base(options)
         {
         }
-    }
+
+        public DbSet <Deposito> Depositos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }  
 }
