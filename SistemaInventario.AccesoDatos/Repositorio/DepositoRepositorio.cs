db4f65d88f460650f299e4e62bbcd9d247322f16
IvanCarrieri <ivancarrieri@yahoo.com.ar>
2023-11-27 09:44:17 -0300

    Capa Acceso a datos

---
+++ SistemaInventario.AccesoDatos/Repositorio/DepositoRepositorio.cs
@@ -0,0 +1,38 @@
+﻿using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    internal class DepositoRepositorio : Repositorio<Deposito>, IDepositoRepositorio
+    {
+        private ApplicationDbContext db;
+
+        public DepositoRepositorio(ApplicationDbContext db):base(db) 
+        {
+            this.db = db;
+            
+        }
+
+        public void Actualizar(Deposito deposito)
+        {
+            var depositoBD = db.Depositos.FirstOrDefault(d=>d.Id == deposito.Id);
+
+            if(depositoBD != null)
+            {
+                depositoBD.Nombre= deposito.Nombre;
+                depositoBD.Descripcion= deposito.Descripcion;
+                depositoBD.Estado= deposito.Estado;
+
+                db.SaveChanges();
+
+
+            }
+        }
+    }
+}
---
+++ SistemaInventario.AccesoDatos/Repositorio/UnidadTrabajo.cs
@@ -0,0 +1,35 @@
+﻿using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class UnidadTrabajo : IUnidadTrabajo
+    {
+
+        private ApplicationDbContext db;
+        public IDepositoRepositorio Deposito { get; private set; }
+
+        public UnidadTrabajo(ApplicationDbContext db)
+        {
+            this.db = db;
+            Deposito = new DepositoRepositorio(_context);
+        }
+
+
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public async Task Guardar()
+        {
+            await db.SaveChangesAsync(); 
+        }
+    }
+}
